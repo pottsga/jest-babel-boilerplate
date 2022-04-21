e3dfb6bdda1fba4cd974a024d9747f9b08d3a452
Greg Potts <greg.potts@ellucian.com>
2022-04-21 09:13:30 -0400

    Updated codebase
    - Updated functions to fat arrow functions

--- src/sum.js
+++ src/sum.js
@@ -1,4 +1,4 @@
-function add(a, b) {
+const add = (a, b) => {
     return a + b;
 }
 
--- src/sum.test.js
+++ src/sum.test.js
@@ -1,7 +1,7 @@
 import add from "./sum";
 
 describe('sum.js', function () {
-    test('add two numbers', function () {
+    test('add two numbers', () => {
         expect(add(1, 2)).toBe(3);
     })
 })
